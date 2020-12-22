b8b004710051b2ce8ac1a0ee8609b7da7769cfe5
alepetpan <alepetpan@ece.auth.gr>
2020-12-22 19:12:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
-<?php
-	echo "Hello";
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Secretary's Login Page</title>
+	</head>
+	<style>
+		body{
+			font-family: Georgia;
+			border: 4px solid white;
+			border-radius: 10px;
+			padding: 100px;
+			background-color: lightblue;
+			
+		}
+		h1, h2{
+			color: blue;
+			font-size: 300%;
+			text-align: center;
+		}
+		p{
+			color: black;
+			font-family: Georgia;
+			text-align: center;
+		}
+		img{
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+			border: 2px outset;
+	
+		}
+		form{
+			text-align: center;
+		}
+		
+		button{
+			text-align: center;
+		}
+	</style>
+	<body>
+		<a href="firstPage.html">firstPage</a>
+		<?php
+			echo "Hello";	
+		?>
+	</body>
+</html>
