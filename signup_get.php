c66d60c332212ca94d9c70074cc839c388e2a75c
alepetpan <alepetpan@ece.auth.gr>
2020-12-22 20:53:59 +0200

    .

---
+++ signup_get.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Secretary's Sign Up Page</title>
+	</head>
+	<style>
+		body{
+			font-family: Georgia;
+			border: 4px solid white;
+			border-radius: 10px;
+			padding: 100px;
+			background-color: lightblue;
+			text-align: center		
+		}
+	</style>
+	<body>
+		<?php
+			include "config.php";
+			$conn -> select_db("mydb");
+			
+			$username = $_POST["username"];
+			$password = $_POST["password"];
+			$firstname = $_POST["firstname"];
+			$lastname = $_POST["lastname"];
+			
+			if (($username!=NULL) && ($password!=NULL)){
+				$sql = "INSERT INTO registration (username, password, firstname, lastname)
+				VALUES ('$username', '$password', '$firstname', '$lastname')";
+				
+				if (mysqli_query($conn, $sql) && (($username)!= NULL) && (($password) != NULL)) {
+					echo "New record created successfully. <br>Redirecting to Login Page.";
+				?>
+				<script>
+					setTimeout(function() {
+						window.location.href="login.html";
+					}, 3000);
+				</script> 	
+				<?php
+				} else {
+					echo mysqli_error($conn);
+				}
+			}
+			else
+				echo "Username and Password cannot left empty";
+			mysqli_close($conn);			
+		
+		?>
+	</body>
+</html>
+
+
+
