9b98c45d345e57a91e7bd2c533d2d2a058dd5b4e
alepetpan <alepetpan@ece.auth.gr>
2021-01-08 02:09:56 +0200

    .

--- secNewCust.php
+++ secNewCust.php
@@ -15,27 +15,32 @@
 			$fullname = $_POST["fullname"];
 			$phone = $_POST["phone"];
 			$age = $_POST["age"];
-			$sql = "INSERT INTO Customer(full_name, phone_number, age)
-						VALUES ('$fullname', '$phone', '$age')";
-			if (mysqli_query($conn, $sql)){
-				echo "New record created successfully. <br>Redirecting to Secretery's new Customer Registration Page.";
-						?>
-						<script>
-							setTimeout(function() {
-								window.location.href="secretaryNewCust.php";
-							}, 3000);
-						</script> 	
-						<?php
-			}else {
-					echo mysqli_error($conn);
-				?>
-				<script>
-					setTimeout(function() {
-						window.location.href="secretaryNewCust.php";
-					}, 3000);
-				</script> 	
-				<?php
-				}
+			if(!empty($full_name) && !empty($phone) && !empty($age)){
+				$sql = "INSERT INTO Customer(full_name, phone_number, age)
+							VALUES ('$fullname', '$phone', '$age')";
+				if (mysqli_query($conn, $sql)){
+					echo "New record created successfully. <br>Redirecting to Secretery's new Customer Registration Page.";
+							?>
+							<script>
+								setTimeout(function() {
+									window.location.href="secretaryNewCust.php";
+								}, 3000);
+							</script> 	
+							<?php
+				}else {
+						echo mysqli_error($conn);
+					?>
+					<script>
+						setTimeout(function() {
+							window.location.href="secretaryNewCust.php";
+						}, 3000);
+					</script> 	
+					<?php
+					}
+			} else{
+				echo "Please fill all fields";
+			}
+
 			
 		?>
 	</body>
--- secNewRes.php
+++ secNewRes.php
@@ -16,28 +16,31 @@
 			$customerID = $_POST["customerID"];
 			$price = $_POST["price"];
 			$seat_num = $_POST["seat_num"];
-			$sql = "INSERT INTO reservation(Trip_tripID, Customer_customerID, price, seat_num)
-						VALUES ('$tripID', '$customerID', '$price', '$seat_num')";
-			if (mysqli_query($conn, $sql)){
-				echo "New record created successfully. <br>Redirecting to Secretery's new Reservation Page.";
-						?>
-						<script>
-							setTimeout(function() {
-								window.location.href="secretaryNewRes.php";
-							}, 3000);
-						</script> 	
-						<?php
-			}else {
-					echo mysqli_error($conn);
-				?>
-				<script>
-					setTimeout(function() {
-						window.location.href="secretaryNewRes.php";
-					}, 3000);
-				</script> 	
-				<?php
-				}
-			
+			if(!empty($tripID) && !empty($customerID) && !empty($price) && !empty($seat_num)){
+				$sql = "INSERT INTO reservation(Trip_tripID, Customer_customerID, price, seat_num)
+							VALUES ('$tripID', '$customerID', '$price', '$seat_num')";
+				if (mysqli_query($conn, $sql)){
+					echo "New record created successfully. <br>Redirecting to Secretery's new Reservation Page.";
+							?>
+							<script>
+								setTimeout(function() {
+									window.location.href="secretaryNewRes.php";
+								}, 3000);
+							</script> 	
+							<?php
+				}else {
+						echo mysqli_error($conn);
+					?>
+					<script>
+						setTimeout(function() {
+							window.location.href="secretaryNewRes.php";
+						}, 3000);
+					</script> 	
+					<?php
+					}
+			}else{
+				echo "Please fill all fields";
+			}
 		?>
 	</body>
 </html>
