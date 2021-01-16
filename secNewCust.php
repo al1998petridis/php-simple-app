6b3581106fc02001177498864a9df256f2a8ecff
alepetpan <alepetpan@ece.auth.gr>
2021-01-16 19:20:24 +0200

    Update secNewCust.php

--- secNewCust.php
+++ secNewCust.php
@@ -15,7 +15,7 @@
 			$fullname = $_POST["fullname"];
 			$phone = $_POST["phone"];
 			$age = $_POST["age"];
-			if(!empty($full_name) && !empty($age)){
+			if(!empty($full_name)){
 				$sql = "INSERT INTO Customer(full_name, phone_number, age)
 							VALUES ('$fullname', '$phone', '$age')";
 				if (mysqli_query($conn, $sql)){
